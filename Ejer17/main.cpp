c93cceaac1d5091e2cc2daa76348e37ef0380923
Jhonathan Otamendi <jhonathanguerrero030@gmail.com>
2024-07-03 15:51:21 -0400

    Ver 4

--- Ejer17/main.cpp
+++ Ejer17/main.cpp
@@ -3,13 +3,21 @@
 
 #include <iostream>
 #include <conio.h>
+#include <stdlib.h>
 using namespace std;
 
 
 int main() {
+	int a;
 
+	a = 10;
 
+	do {
+		cout << a << endl;
+		a--;
+	} while (a >= 1);
 
+	system("pause");
 	cin.get();
 	return 0;
 }
